1929312dbfda479697741566d42fb448a7b8dc6f
Neal Fultz <nfultz@gmail.com>
2017-03-17 20:56:18 +0000

    Make it predictable.

--- R/hashtrick.R
+++ R/hashtrick.R
@@ -1,26 +1,33 @@
 #' Hash Trick Functions for Model Building
 #'
-#' \code{hashtrick} builds a model matrix using the hash trick, \code{hash_col} hashes a single column.
+#' \code{hashtrick} builds a matrix using the hash trick, \code{hash_col} hashes a single column.
 #'
 #' @param ... a list of variables
 #' @param x character vector to hash
-#' @param m modulo - an integer
+#' @param m modulo - number of columns for result
 #' @param algo \code{'md5'} and others provided by OpenSSL
-#' @param key a raw vector. If provided, the hash function generates a different mapping
+#' @param key a list of raws. If provided, the hash function generates a different mapping
+#'
+#' @return for \code{hashtrick}, a n-by-m matrix of the dummy coded features. For \code{hash_col},
+#'   a matrix containing hashes, one column per key provided.
 #'
 #' @export
 #' @importFrom Matrix sparseMatrix t
 #' @rdname hashtrick
-hashtrick <- function(..., m=256L, algo='md5', key) {
-  res <- list(...)
-  n <- length(res[[1]])
+hashtrick <- function(..., m=256L, algo='md5', key=DEFAULT_KEY) {
+  stopifnot(is.integer(m))
+  vars <- list(...)
+  n <- length(vars[[1]])
+  out <- matrix(0L, n, m, dimnames = list(names(vars[[1]]), 1:m))
 
-  res <- lapply(res, hash_col, m, algo, key)
+  for(v in vars)
+    mark(hash_col(v, m, algo, key), out)
 
-  res <- lapply(res, sparseMatrix, j=0:(n-1), dims=c(m,n), index1=FALSE)
+  structure(out, m=m, algo=algo, key=key, class='hashtrick')
 
-  t(Reduce(`+`, res))
 }
+
+DEFAULT_KEY=list(charToRaw('package:hashtrick'))
 
 #' @export
 #' @rdname hashtrick
@@ -30,17 +37,21 @@ hash_col <- function(x, m, algo, key) {
 
 #' @export
 #' @rdname hashtrick
-hash_col.default <- function(x, m, algo, key) {
-  if(missing(key))
-    digest(x, m, algo, key)
-  else
+hash_col.character <- function(x, m, algo, key) {
     hmac(x, m, algo, key)
 }
 
 #' @export
 #' @rdname hashtrick
+hash_col.numeric <- function(x, m, algo, key) {
+    hmac(as.character(x), m, algo, key)
+}
+
+
+#' @export
+#' @rdname hashtrick
 hash_col.factor <- function(x, m, algo, key) {
-   hash_col(levels(x), m, algo, key)[x]
+   hmac(levels(x), m, algo, key)[x,]
 }
 
 #' Raw hash functions
@@ -50,15 +61,37 @@ hash_col.factor <- function(x, m, algo, key) {
 #' @param x     character vector to hash
 #' @param m     modulo - an integer
 #' @param algo \code{'md5'} and others provided by OpenSSL
-#' @param key   a raw vector. If provided, generates a different mapping
-#'
-#' @useDynLib hashtrick R_digest
-#' @export
-#' @rdname digest
-digest <- function(x, m, algo, ...) .Call(R_digest, x, m, algo)
-
+#' @param key   a list of raw vectors. Each specifies a different mapping
 #' @useDynLib hashtrick R_hmac
-#' @export
-#' @rdname digest
 hmac <- function(x, m, algo, key) .Call(R_hmac, x, m, algo, key)
 
+
+#' @useDynLib hashtrick R_mark_matrix
+mark <- function(idx, out) .Call(R_mark_matrix, idx, out)
+
+
+
+#' @export
+makepredictcall.hashtrick <- function(var, call){
+    # Stolen from splines package
+    if (as.character(call)[1L] != "hashtrick")
+        return(call)
+    args <- c("m", "algo", "key")
+
+
+    at <- attributes(var)[args]
+    xxx <- call
+    xxx[args] <- NULL
+    xxx[names(at)] <- at
+    xxx
+}
+
+#' @export
+predict.hashtrick <- function(object, newx, ...)
+{
+    if(missing(newx))
+        return(object)
+    a <- c(list(x = newx), attributes(object)[
+                c("m", "algo", "key")])
+    do.call("hashtrick", a)
+}
--- src/hashtrick.c
+++ src/hashtrick.c
@@ -7,82 +7,77 @@
 
 
 
+SEXP R_hmac(SEXP x, SEXP mod, SEXP algo, SEXP keys){
 
-SEXP R_digest(SEXP x, SEXP mod, SEXP algo){
   const EVP_MD *md = EVP_get_digestbyname(CHAR(asChar(algo)));
-
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+  unsigned int md_len;
 
   int len = length(x);
-  SEXP out = PROTECT(allocVector(INTSXP, len));
+  int K = length(keys);
+  SEXP out = PROTECT(allocMatrix(INTSXP, len, K));
   int *is = INTEGER(out);
-
-  BN_ULONG m = asInteger(mod);
 
   BIGNUM *val = BN_new();
 
-  unsigned char md_value[EVP_MAX_MD_SIZE];
-  unsigned int md_len;
-  for (int i = 0; i < len; i++) {
-    /* check for NA */
-    if(STRING_ELT(x, i) == NA_STRING) {
-      is[i] = m-1;
-      continue;
+  BN_ULONG m = INTEGER(mod)[0];
+
+
+  for(int k = 0; k < K; k++){
+
+    const unsigned char* rawkey = RAW(VECTOR_ELT(keys, k));
+    const int keylen = LENGTH(VECTOR_ELT(keys, k));
+
+    for (int i = 0; i < len; i++, is++) {
+      /* check for NA */
+      if(STRING_ELT(x, i) == NA_STRING) {
+        is[i] = m-1;
+        continue;
+      }
+      /* create hash */
+      const char* str = CHAR(STRING_ELT(x, i));
+      int stringlen = LENGTH(STRING_ELT(x, i));
+
+      HMAC(md, rawkey, keylen, (unsigned char*) str, stringlen, md_value, &md_len);
+
+      BN_bin2bn(md_value, md_len, val);
+      /* mod */
+      *is = (int) BN_mod_word(val, m);
     }
-    /* create hash */
-    const char* str = CHAR(STRING_ELT(x, i));
-    int stringlen = LENGTH(STRING_ELT(x, i));
-
-    EVP_Digest((unsigned char*) str, stringlen, md_value, &md_len, md, NULL);
-
-    BN_bin2bn(md_value, md_len, val);
-    /* mod */
-    is[i] = (int) BN_mod_word(val, m);
-
   }
   free(val);
+
+  /*/ Set dimension on output
+  SEXP t = PROTECT(t = allocVector(INTSXP, 2));
+  INTEGER(t)[0] = len;
+  INTEGER(t)[1] = K;
+  setAttrib(out, R_DimSymbol, t);
+  UNPROTECT(1);*/
   UNPROTECT(1);
   return out;
 }
 
-SEXP R_hmac(SEXP x, SEXP mod, SEXP algo, SEXP key){
+SEXP R_mark_matrix(SEXP idx, SEXP out){
 
-  const EVP_MD *md = EVP_get_digestbyname(CHAR(asChar(algo)));
+  int *dims;
+
+  dims = INTEGER(coerceVector(getAttrib(idx, R_DimSymbol), INTSXP));
+  int n = dims[0], p = dims[1];
+  dims = INTEGER(coerceVector(getAttrib(idx, R_DimSymbol), INTSXP));
+  int m = dims[1];
+
+  int *x = INTEGER(out);
+  int *q = INTEGER(idx);
+  //Rprintf("%d %d %d\n", p, n, m);
 
 
-  int len = length(x);
-  SEXP out = PROTECT(allocVector(INTSXP, len));
-  int *is = INTEGER(out);
-
-  const unsigned char* rawkey = RAW(key);
-  const int keylen = LENGTH(key);
-
-
-
-  BN_ULONG m = asInteger(mod);
-
-  BIGNUM *val = BN_new();
-
-  unsigned char md_value[EVP_MAX_MD_SIZE];
-  unsigned int md_len;
-  for (int i = 0; i < len; i++) {
-    /* check for NA */
-    if(STRING_ELT(x, i) == NA_STRING) {
-      is[i] = m-1;
-      continue;
+  for(int j = 0; j < p; j++)
+    for(int i = 0; i < n; i++, q++) {
+      //Rprintf("%d %d %d %d\n", i, j, *q, *q * n + i);
+      x[*q * m + i]++;
     }
-    /* create hash */
-    const char* str = CHAR(STRING_ELT(x, i));
-    int stringlen = LENGTH(STRING_ELT(x, i));
 
-    HMAC(md, rawkey, keylen, (unsigned char*) str, stringlen, md_value, &md_len);
 
-    BN_bin2bn(md_value, md_len, val);
-    /* mod */
-    is[i] = (int) BN_mod_word(val, m);
-
-  }
-  free(val);
-  UNPROTECT(1);
-  return out;
+  return(out);
 }
 
